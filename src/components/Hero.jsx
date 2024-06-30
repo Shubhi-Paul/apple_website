e7b4cb9ba64e717c243ac3f95ef79fe34a7ee684
shubhipaul <shubhipaul@gmail.com>
2024-07-01 00:24:06 +0530

    Video Carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,28 +29,39 @@ const Hero = () => {
 
   useGSAP(() => {
     gsap.to('#hero', { opacity: 1, delay: delayTime })
-    gsap.fromTo('#cta', {y:20},{ opacity: 1, y: 0, delay: delayTime })
+    gsap.to('#cta',{ opacity: 1, y: -50, delay: delayTime })
   }, [])
 
   return (
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
-        <p id="hero" className="hero-title">iPhone 15 Pro</p>
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
         <div className="md:w-10/12 w-9/12">
-          <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+          <video
+            className="pointer-events-none"
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+
       <div
         id="cta"
         className="flex flex-col items-center opacity-0 translate-y-20"
       >
-        <a href="#highlights" className="btn">Buy</a>
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
         <p className="font-normal text-xl">From $199/month or $999</p>
       </div>
-      </div>
     </section>
-  )
+  );
 }
 
 export default Hero
